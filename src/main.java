28174e44389e467b4b5aae06f275dfe8b1318877
unknown <dijesh.poyyara@gmail.com>
2020-10-24 10:42:39 -0400

    Main class

---
+++ src/main.java
@@ -0,0 +1,14 @@
+public class main {
+    public static void main(String[] args) {
+        Question10 q10 = new Question10();
+        int inputArr[] = {1,2 , 3, 4, 5};
+        q10.reverseArray(inputArr);
+
+
+        Question11 q11 = new Question11();
+        q11.isPalindrome("abcddcba");
+
+        Question12 q12 = new Question12();
+        q12.onlyDigits("12312312");
+    }
+}
